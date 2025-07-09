6dfa637bf4ede61a3ce3c0b49f9afed64a940cc6
priatnahrn <herlinpriatna31@gmail.com>
2025-07-09 19:45:15 +0700

    update

--- Modules/Auth/app/Transformers/AuthResource.php
+++ Modules/Auth/app/Transformers/AuthResource.php
@@ -4,6 +4,7 @@ namespace Modules\Auth\Transformers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Modules\Profile\Transformers\ProfileMasyarakatResource;
 
 class AuthResource extends JsonResource
 {
@@ -20,7 +21,7 @@ class AuthResource extends JsonResource
             'username'     => $this->username ?? null,   // untuk internal user
             'email'        => $this->email ?? null,      // untuk staff/kepala desa
             'roles'        => $this->roles->pluck('name'), // hanya ambil nama rolenya
-            'profile_masyarakat' => $this->whenLoaded('profileMasyarakat'), // ✅ tambahkan ini
+            'profile_masyarakat' => new ProfileMasyarakatResource($this->profileMasyarakat), // ✅ tambahkan ini
         ];
     }
 }
