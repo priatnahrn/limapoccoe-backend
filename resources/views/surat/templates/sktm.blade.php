580972252ef0bbd0b317bb3cee7c030fec837934
priatnahrn <herlinpriatna31@gmail.com>
2025-07-11 14:25:33 +0700

    UP SKTM

--- resources/views/surat/templates/sktm.blade.php
+++ resources/views/surat/templates/sktm.blade.php
@@ -6,28 +6,25 @@
     <style>
         body {
             font-family: 'Times New Roman', Times, serif;
-            line-height: 1.6;
+            line-height: 1.5;
             margin: 30px;
         }
         .center { text-align: center; }
         .bold { font-weight: bold; }
         .mt-3 { margin-top: 1rem; }
-        .mt-5 { margin-top: 3rem; }
+        .mt-4 { margin-top: 2rem; }
         .text-right { text-align: right; }
         .indent { text-indent: 2em; }
-        table { page-break-inside: avoid; }
+        table { width: 100%; page-break-inside: avoid; }
+        td { vertical-align: top; }
     </style>
 </head>
 <body>
 
-    <table width="100%">
+    <table>
         <tr>
-            <td style="width: 100px;">
-              <img
-                    src="{{ $isPreview ? asset('logo-limapoccoe.png') : public_path('logo-limapoccoe.png') }}"
-                    alt="Logo Desa"
-                    style="height: 90px;">
-
+            <td style="width: 90px;">
+                <img src="{{ $isPreview ? asset('logo-limapoccoe.png') : public_path('logo-limapoccoe.png') }}" alt="Logo" style="height: 85px;">
             </td>
             <td class="center">
                 <div class="bold">PEMERINTAH DESA LIMAPOCCOE</div>
@@ -42,21 +39,22 @@
     <hr>
 
     <div class="center">
-        <h2><u>SURAT KETERANGAN TIDAK MAMPU</u></h2>
+        <h3><u>SURAT KETERANGAN TIDAK MAMPU</u></h3>
         <div>Nomor: {{ $ajuan->nomor_surat_tersimpan ?? '___/SKTM/___/__/____' }}</div>
     </div>
 
     <p class="mt-3">Yang bertanda tangan di bawah ini:</p>
     <table>
-        <tr><td>Nama</td><td>: {{ $ajuan->tandaTangan->user->name ?? 'H ANDI ABU BAKRI' }}</td></tr>
-        <tr><td>Jabatan</td><td>: {{ 'Kepala Desa Limapoccoe' }}</td></tr>
+        <tr><td style="width: 150px;">Nama</td><td>: {{ $ajuan->tandaTangan->user->name ?? 'H ANDI ABU BAKRI' }}</td></tr>
+        <tr><td>Jabatan</td><td>: Kepala Desa Limapoccoe</td></tr>
     </table>
 
     <p class="mt-3">Menerangkan bahwa:</p>
     <table>
-        <tr><td>Nama</td><td>: {{ $user->name ?? $data['nama'] ?? '-' }}</td></tr>
+        <tr><td style="width: 150px;">Nama</td><td>: {{ $user->name ?? $data['nama'] ?? '-' }}</td></tr>
         <tr><td>NIK</td><td>: {{ $user->nik ?? $data['nik'] ?? '-' }}</td></tr>
-        <tr><td>Tempat/Tanggal Lahir</td>
+        <tr>
+            <td>Tempat/Tanggal Lahir</td>
             <td>: {{ optional($profile)->tempat_lahir ?? $data['tempat_lahir'] ?? '-' }},
                  {{ \Carbon\Carbon::parse(optional($profile)->tanggal_lahir ?? $data['tanggal_lahir'] ?? now())->format('d-m-Y') }}
             </td>
@@ -68,10 +66,10 @@
 
     <p class="mt-3">Anak dari Pasangan:</p>
     <table>
-        <tr><td>Nama Ayah</td><td>: {{ $data['nama_ayah'] ?? '-' }}</td></tr>
-        <tr><td>Pekerjaan</td><td>: {{ $data['pekerjaan_ayah'] ?? '-' }}</td></tr>
+        <tr><td style="width: 150px;">Nama Ayah</td><td>: {{ $data['nama_ayah'] ?? '-' }}</td></tr>
+        <tr><td>Pekerjaan Ayah</td><td>: {{ $data['pekerjaan_ayah'] ?? '-' }}</td></tr>
         <tr><td>Nama Ibu</td><td>: {{ $data['nama_ibu'] ?? '-' }}</td></tr>
-        <tr><td>Pekerjaan</td><td>: {{ $data['pekerjaan_ibu'] ?? '-' }}</td></tr>
+        <tr><td>Pekerjaan Ibu</td><td>: {{ $data['pekerjaan_ibu'] ?? '-' }}</td></tr>
         <tr><td>Jumlah Tanggungan</td><td>: {{ $data['jumlah_tanggungan'] ?? '-' }}</td></tr>
     </table>
 
@@ -83,37 +81,31 @@
         Demikian surat keterangan ini kami buat dengan sebenarnya untuk digunakan seperlunya.
     </p>
 
-    <div class="mt-5" style="display: flex; justify-content: space-between; align-items: flex-start;">
-        {{-- QR Code --}}
-       {{-- Tampilkan SVG di preview
-       {{-- Tampilkan QR Code --}}
-        @if($isPreview)
-            {!! $qrCodeSvg ?? '' !!}
-        @elseif(isset($qrCodePath) && file_exists($qrCodePath))
-            <img src="file://{{ $qrCodePath }}" style="width: 60px;" alt="QR Code">
+    <div class="mt-4" style="display: flex; justify-content: space-between;">
+        @if(isset($qrCodePath) && file_exists($qrCodePath))
+            <div>
+                <img src="file://{{ $qrCodePath }}" style="width: 60px;" alt="QR Code">
+            </div>
+        @else
+            <div></div>
         @endif
 
-
-
-
-        {{-- Tanda Tangan --}}
-        <div style="text-align: right; width: 50%;">
+        <div style="text-align: center; width: 45%;">
             <div>Limapoccoe, {{ \Carbon\Carbon::parse($data['tanggal_surat'] ?? now())->translatedFormat('d F Y') }}</div>
-            <div>KEPALA DESA LIMAPOCCOE</div>
-            <div>KEPALA DESA</div>
+            <div class="bold mt-1">KEPALA DESA LIMAPOCCOE</div>
 
-            <div style="margin-top: 20px;">
+            <div style="margin-top: 10px;">
                 @php
                     $ttdPath = storage_path('app/private/tanda-tangan-digital.png');
                     $ttdBase64 = file_exists($ttdPath) ? base64_encode(file_get_contents($ttdPath)) : null;
                 @endphp
 
                 @if ($ajuan->status === 'approved' && $ttdBase64)
-                    <img src="data:image/png;base64,{{ $ttdBase64 }}" style="height: 200px;" alt="Tanda Tangan"><br>
-                    <strong>{{ $ajuan->tandaTangan->user->name ?? 'H ANDI ABU BAKRI' }}</strong>
+                    <img src="data:image/png;base64,{{ $ttdBase64 }}" style="height: 120px;" alt="Tanda Tangan"><br>
+                    <strong style="display: inline-block; margin-top: -10px;">{{ $ajuan->tandaTangan->user->name ?? 'H ANDI ABU BAKRI' }}</strong>
                 @else
                     <div style="height: 100px;"></div>
-                    <strong style="color: grey">Belum ditandatangani</strong>
+                    <strong style="color: grey;">Belum ditandatangani</strong>
                 @endif
             </div>
         </div>
@@ -124,9 +116,9 @@
     </div>
 
     @if($ajuan->status === 'approved')
-        <hr>    
+        <hr>
         <div class="text-right">
-            <p><small>Dicetak pada: {{ now()->translatedFormat('d F Y H:i') }}</small></p>
+            <small>Dicetak pada: {{ now()->translatedFormat('d F Y H:i') }}</small>
         </div>
     @endif
 
