bfb544d9f4d9762ccb874554f1a74b431e743653
priatnahrn <herlinpriatna31@gmail.com>
2025-08-20 10:13:59 +0700

    up

--- Modules/DataKependudukan/routes/api.php
+++ Modules/DataKependudukan/routes/api.php
@@ -19,5 +19,10 @@ Route::middleware(['auth:api'])->group(function () {
 Route::prefix('statistik')->group(function () {
     Route::get('/jumlah-penduduk', [DataKependudukanController::class, 'getJumlahPenduduk']);
     Route::get('/jumlah-keluarga', [DataKependudukanController::class, 'getJumlahKeluarga']);
-    Route::get('/jumlah-jenis-kelamin', [DataKependudukanController::class, 'getJumlahPerJenisKelamin']);
+    Route::get('/jumlah-per-jenis-kelamin', [DataKependudukanController::class, 'getJumlahPendudukPerJenisKelamin']);
+    Route::get('/jumlah-per-agama', [DataKependudukanController::class, 'getJumlahPendudukPerAgama']);
+    Route::get('/jumlah-per-pendidikan', [DataKependudukanController::class, 'getJumlahPendudukPerPendidikan']);
+    Route::get('/jumlah-per-pekerjaan', [DataKependudukanController::class, 'getJumlahPendudukPerPekerjaan']);
+    Route::get('/jumlah-per-status-perkawinan', [DataKependudukanController::class, 'getJumlahPendudukPerPerkawinan']);
+    Route::get('/jumlah-per-dusun', [DataKependudukanController::class, 'getJumlahPendudukPerDusun']);
 });
